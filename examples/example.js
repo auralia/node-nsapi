538d2379e270dae998cec3f174325388c72c843e
Michel Nguyen <modzongaming@gmail.com>
2017-12-27 16:11:19 +0100

    update examples

--- examples/example.js
+++ examples/example.js
@@ -14,62 +14,72 @@
  * limitations under the License.
  */
 
-var nsapi = require("../lib/api.js");
+const { NsApi, WorldAssemblyCouncil, TelegramType } = require("../lib/api.js");
 
-// TODO: Replace the user agent with your own
-var api = new nsapi.NsApi("Your nation's name");
+/**
+ * TODO: Replace the user agent with your own
+ */
+const api = new NsApi("Your nation's name");
 
-// The following is a simple example that retrieves the nation Auralia's full
-// name and prints it to the console.
+/**
+ * The following is a simple example that retrieves the nation Auralia's full
+ * name and prints it to the console.
+ */
 function nationApiExample() {
     return api.nationRequest("Auralia", ["fullname"])
-              .then(function(data) {
-                  console.log(data["fullname"]);
-              });
+        .then(data => {
+            console.log(data.fullname);
+        });
 }
 
-// The following is the same example as example 1, but notice how it completes
-// much faster because the previous request was cached!
+/**
+ * The following is the same example as example 1, but notice how it completes
+ * much faster because the previous request was cached!
+ */
 function cacheExample() {
     return nationApiExample();
 }
 
-// The following example retrieves the delegate, founder, and list of nations
-// in the region of Catholic.
+/**
+ * The following example retrieves the delegate, founder, and list of nations
+ * in the region of Catholic.
+ */
 function regionApiExample() {
     return api.regionRequest("Catholic", ["nations", "delegate", "founder"])
-              .then(function(data) {
-                  console.log("Region of Catholic");
-                  console.log();
-                  console.log("Delegate: " + data["delegate"]);
-                  console.log("Founder: " + data["founder"]);
-                  console.log("Nations: " + data["nations"].split(":"));
-              })
+        .then(data => {
+            console.log(`
+Region of Catholic
+
+Delegate: ${data.delegate}
+Founder: ${data.founder}
+Nations: ${data.ntaions.split(":")}
+            `);
+        });
 }
 
 // The following example retrieves the last 5 founding happening entries.
 function worldApiExample() {
     return api.worldRequest(["happenings"],
-                            {filter: "founding", limit: "5"})
-              .then(function(data) {
-                  for (var i = 0; i < data["happenings"]["event"].length; i++) {
-                      var event = data["happenings"]["event"][i];
-                      console.log();
-                      console.log("Event ID: " + event["id"]);
-                      console.log("Event Timestamp: " + event["timestamp"]);
-                      console.log("Event Text: " + event["text"]);
-                  }
-              })
+        { filter: "founding", limit: "5" })
+        .then(data => {
+            for (const event of data.happenings.event) {
+                console.log(`
+Event ID: ${event.id}
+Event Timestamp: ${event.timestamp}
+Event Text: ${event.text}
+                `);
+            }
+        });
 }
 
 // The following example retrieves information about the last Security Council
 // resolution at vote.
 function worldAssemblyApiExample() {
-    return api.worldAssemblyRequest(nsapi.WorldAssemblyCouncil.SecurityCouncil,
-                                    ["lastresolution"])
-              .then(function(data) {
-                  console.log(data["lastresolution"]);
-              });
+    return api.worldAssemblyRequest(WorldAssemblyCouncil.SecurityCouncil,
+        ["lastresolution"])
+        .then(data => {
+            console.log(data.lastresolution);
+        });
 }
 
 // The following is a complex example that retrieves and sorts the list of
@@ -77,39 +87,35 @@ function worldAssemblyApiExample() {
 // list to the console.
 function complexExample() {
     return api.regionRequest("Catholic", ["nations"])
-              .then(function(data) {
-                  var nations = data["nations"].split(":");
+        .then(data => {
+            const nations = data.nations.split(":");
 
-                  return Promise.all(nations.map(function(nation) {
-                      return api.nationRequest(
-                          nation,
-                          ["name", "censusscore-65"]
-                      ).then(function(data) {
-                          console.log(
-                              "Retrieved information for " + data["name"]
-                              + ".");
-                          return {
-                              nation: data["name"],
-                              influence: data["censusscore"]["value"]
-                          };
-                      });
-                  }));
-              })
-              .then(function(influence) {
-                  influence.sort(function(a, b) {
-                      return b["influence"] - a["influence"];
-                  });
+            return Promise.all(nations.map(nation => {
+                return api.nationRequest(
+                    nation,
+                    ["name", "censusscore-65"]
+                ).then(data => {
+                    console.log(`Retrieved information for ${data.name}`);
+                    return {
+                        nation: data.name,
+                        influence: data.censusscore.value
+                    };
+                });
+            }));
+        })
+        .then(influence => {
+            influence.sort((a, b) => {
+                return b.influence - a.influence;
+            });
 
-                  console.log("\n" + rightpad("Nation", 50) + " Influence");
-                  for (var i = 0; i < influence.length; i++) {
-                      console.log(rightpad(influence[i].nation, 50) + " "
-                                  + influence[i].influence);
-                  }
-              });
-
+            console.log(`\n${rightpad("Nation", 50)} Influence"`);
+            for (let i = 0; i < influence.length; i++) { // eslint-disable-line id-length
+                console.log(`${rightpad(influence[i].nation, 50)} ${influence[i].influence}`);
+            }
+        });
     function rightpad(str, num) {
-        var len = str.length;
-        for (var i = 0; i < num - len; i++) {
+        const len = str.length;
+        for (let id = 0; id < num - len; id++) {
             str += " ";
         }
         return str;
@@ -117,45 +123,45 @@ function complexExample() {
 }
 
 function telegramExample() {
-    // TODO: Replace telegram details with your own
-    var clientKey = "";
-    var telegramId = "";
-    var telegramSecretKey = "";
+    /** Replace telegram details with your own */
+    const clientKey = "";
+    const telegramId = "";
+    const telegramSecretKey = "";
 
     return api.telegramRequest(clientKey, telegramId, telegramSecretKey,
-                               "Auralia", nsapi.TelegramType.NonRecruitment)
-              .then(function() {
-                  console.log("Telegram sent");
-              })
-              .catch(function(err) {
-                  console.log("Telegram was not sent: " + err);
-              });
+        "Auralia", TelegramType.NonRecruitment)
+        .then(() => {
+            console.log("Telegram sent");
+        })
+        .catch(err => {
+            console.log("Telegram was not sent", err);
+        });
 }
 
 function authenticationExample() {
-    // TODO: Replace nation name and checksum with your own
-    var nation = "";
-    var checksum = "";
+    /** Replace nation name and checksum with your own */
+    const nation = "";
+    const checksum = "";
 
     return api.authenticateRequest(nation, checksum)
-              .then(function(success) {
-                  if (success) {
-                      console.log("Authentication succeeded");
-                  } else {
-                      console.log("Authentication failed");
-                  }
-              });
+        .then(success => {
+            if (success) {
+                console.log("Authentication succeeded");
+            } else {
+                console.log("Authentication failed");
+            }
+        });
 }
 
 // The following example uses private shards to retrieve the next issue time
 // and print it to the console, along with the PIN required for future private
 // shard requests.
 function privateShardsExample() {
-    // TODO: Replace the nation name and password with your own
-    var nationName = "";
-    var nationPassword = "";
+    /** Replace the nation name and password with your own */
+    const nationName = "";
+    const nationPassword = "";
 
-    var auth = {
+    const auth = {
         password: nationPassword,
         updatePin: true
     };
@@ -164,53 +170,36 @@ function privateShardsExample() {
         ["nextissuetime"],
         undefined,
         auth)
-              .then(function(data) {
-                  console.log("Next issue time: "+ data["nextissuetime"]);
-                  console.log("PIN: " + auth.pin);
-              });
+        .then(data => {
+            console.log(`
+Next issue time: ${data.nextissuetime}
+PIN: ${auth.pin}
+`);
+        });
 }
 
 // The following code executes each example.
-Promise.resolve()
-       .then(function() {
-           console.log("Nation API example:\n");
-           return nationApiExample();
-       })
-       .then(function() {
-           console.log("\nCache example:\n");
-           return cacheExample();
-       })
-       .then(function() {
-           console.log("\nRegion API example:\n");
-           return regionApiExample();
-       })
-       .then(function() {
-           console.log("\nWorld API example:");
-           return worldApiExample();
-       })
-       .then(function() {
-           console.log("\nWorld Assembly API example:\n");
-           return worldAssemblyApiExample();
-       })
-       .then(function() {
-           console.log("\nComplex example:\n");
-           return complexExample();
-       })
-       .then(function() {
-           console.log("\nTelegram example:\n");
-           return telegramExample();
-       })
-       .then(function() {
-           console.log("\nAuthentication example:\n");
-           return authenticationExample();
-       })
-       .then(function() {
-           console.log("\nPrivate shards example:\n");
-           return privateShardsExample();
-       })
-       .catch(function(err) {
-           console.log(err);
-       })
-       .then(function() {
-           api.cleanup();
-       });
+const functions = [
+    console.log("Nation API Example"),
+    nationApiExample(),
+    console.log("Cache Example"),
+    cacheExample(),
+    console.log("Region API Example"),
+    regionApiExample(),
+    console.log("World API Example"),
+    worldApiExample(),
+    console.log("World Assembly API Example"),
+    worldAssemblyApiExample(),
+    console.log("Complex Example"),
+    complexExample(),
+    console.log("Telegram Example"),
+    telegramExample(),
+    console.log("Authentication Example"),
+    authenticationExample(),
+    console.log("Private Shards Example"),
+    privateShardsExample()
+];
+Promise.all(functions)
+    .then(() => {
+        api.cleanup();
+    });
